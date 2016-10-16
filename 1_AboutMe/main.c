3aec958c656062e1d7cedd85712ae99f62c4e18b
Ferdinand Brunauer <ferdinand.brunauer@gmail.com>
2016-10-16 12:09:08 +0200

    Add first project

---
+++ 1_AboutMe/main.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* file: main.c
+*
+* date: 2016-10-10
+* progtimeest.: < 1sec
+* progtimereal: < 1  sec
+* author: Ferdinand Brunauer
+* email: fbrunauer.itsb-b2016@fh-salzburg.ac.at
+*
+* Salzburg University of Applied Sciences
+* Information Technology & Systems Management
+* SWE1-ILV/B, exercise 1
+*/
+
+int main()
+{
+	printf("%s, %s\n", "Brunauer", "Ferdinand");
+	printf("%d\n", 1610555047);
+	printf("%s\n\n", "fbrunauer.itsb-b2016@fh-salzburg.ac.at");
+
+	printf("Finished ...");
+	getchar();
+	return 0;
+}
